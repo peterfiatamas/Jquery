9be5a5e0db7a62fbb9fa46d9ea5ab3c5405ce190
Péterfia Tamás <peterfiathomas@gmail.com>
2017-06-14 19:33:59 +0200

    felugróablak

---
+++ uzenetablak.js
@@ -0,0 +1,14 @@
+function uzenet()
+{
+    $('body').prepend('<div id="uzenet" onclick="bezar()">ergerz</div>');
+    $('#uzenet').after('<div id="ablak">Mauris mattis elementum sem tincidunt dictum.Mauris mattis elementum sem tincidunt dictum.Mauris mattis elementum sem tincidunt dictum.Mauris mattis elementum sem tincidunt dictum.Mauris mattis elementum sem tincidunt dictum.Mauris mattis elementum sem tincidunt dictum.Mauris mattis elementum sem tincidunt dictum. Vivamus et tristique nibh, vitae congue tellus.</div>');
+    /*alert($('#ablak').innerWidth());*/
+    /*var balrol = Math.round($(window).width() - $('#ablak').innerWidth()) / 2;
+    var fentrol = Math.round($(window).height() - $('#ablak').innerHeight()) / 2;
+    alert(fentrol);*/
+}
+function bezar()
+{
+    $('#uzenet').remove();
+    $('#ablak').remove();
+}
